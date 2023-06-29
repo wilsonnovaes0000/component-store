a74392865b8f8d1bc93fe47cf0c1ca75c6ef5f6a
wilsonnovaes0000 <69542312+wilsonnovaes0000@users.noreply.github.com>
2023-06-28 23:56:22 -0300

    3 files modified

---
+++ src/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: 'app-footer',
+  styleUrls: ['./footer.component.scss'],
+  templateUrl: './footer.component.html',
+  standalone: true,
+  encapsulation: ViewEncapsulation.None
+})
+export class FooterComponent {}
